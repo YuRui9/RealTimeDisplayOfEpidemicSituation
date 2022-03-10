88cf1fdc29da81a881097684a21516e41ee8ef2a
tuntun <377394323@qq.com>
2022-03-10 15:08:02 +0800

    测试代码

--- test.py
+++ test.py
@@ -3,9 +3,30 @@
 # @Author : YU RUI
 # @File : test.py
 # @desc :
-def func():
-    print("测试用1")
+def func(num):
+    res_lists = [
+        [1, 3, 2],
+        [6, 4, 5],
+        [9, 8, 7],
+        [0]
+    ]
+    for res_list in res_lists:
+        print(res_list)
+        time = 1
+        for i in res_list:
+            print(i)
+            if int(num) == i:
+                time += 1
+        return time
+
+
+def run():
+    nums = "37259"
+    sum = 0
+    for num in nums:
+        sum += func(num)
+    print(sum)
 
 
 if __name__ == '__main__':
-    func()
+    run()
